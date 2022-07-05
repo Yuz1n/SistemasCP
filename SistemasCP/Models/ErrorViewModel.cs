0a9982faea5eb5ee392cd95584b9e55ea3227017
yuri leite <yuri.rodrigues@lighthouse.com.br>
2022-07-05 17:44:02 -0300

    Projeto Criado

---
+++ SistemasCP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemasCP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
